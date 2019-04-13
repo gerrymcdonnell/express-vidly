67f6a6fa9934f7d6526646222f782361157c8a40
Gerry Mc Donnell <irishbloke@gmail.com>
2019-04-13 16:08:51 +0100

    added morgan for http logging

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
 const mongoose = require('mongoose');
+const morgan = require('morgan');
 
 const genres = require('./routes/genres');
 const customers = require('./routes/customers');
@@ -27,6 +28,17 @@ app.use('/api/movies', movies);
 app.use('/api/rentals', rentals);
 app.use('/api/users', users);
 app.use('/api/auth',auth);
+
+//environments
+console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
+console.log('app.get(env):', app.get('env'));
+
+//morgan middleware logs http requests. only use it development mode
+if (app.get('env') === 'development') {
+  app.use(morgan('tiny'));
+  console.log('Middleware: morgan enabled: Logging Http requests');
+}
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
--- routes/auth.js
+++ routes/auth.js
@@ -23,8 +23,9 @@ router.post('/', async (req, res) => {
     const validPassword=await bcrypt.compare(req.body.password,user.password);
 
     //just for testing
-    console.log('plain text password is: ',req.body.password);
-    console.log('hashed password is: ',user.password);
+    if(router.get('env')==='development')
+        console.log('plain text password is: ',req.body.password);
+        console.log('hashed password is: ',user.password);
 
     if(!validPassword)return res.status(400).send('Invalid email or password');
 
